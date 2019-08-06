852325e77d0a0cb34baf75231b07467378140db1
MagaMiqaelyan <48835022+MagaMiqaelyan@users.noreply.github.com>
2019-08-06 14:18:15 +0400

    1+11+111+...

---
+++ FirstSerie/FirstSerie/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FirstSerie
+{
+    internal class Program
+    {
+        public static int Serie(int n)
+        {
+            int sum = 1, value = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                value = 10 * value + 1;
+                Console.Write(value + " ");
+                sum = sum + value;
+            }
+            return sum;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("THIS IS SERIES 1+11+111+....");
+            Console.Write("The terms of series is ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sum is  " + Serie(n));
+        }
+    }
+}
